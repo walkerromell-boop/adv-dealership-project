ce18a3a81939bfea2de812191bf4a13d95037d0d
Romell Walker <walkerromell@gmail.com>
2025-10-30 11:43:17 -0400

    imported classes from dealership file

---
+++ src/main/java/com/yearup/dealership/Dealership.java
@@ -0,0 +1,93 @@
+package com.yearup.dealership;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> vehicles;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.vehicles = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public List<Vehicle> getVehicleByPrice(double minPrice, double maxPrice) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByMakeModel(String make, String model) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByColor(String color) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByYear(int minYear, int max) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByMileage(int minMiles, int maxMiles) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> getVehicleByType(double minPrice, double maxPrice) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicle() {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> addVehicle(Vehicle newVehicles) {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+    public List<Vehicle> removeVehicle() {
+        List<Vehicle> filtered = new ArrayList<>();
+        return null;
+    }
+
+
+}
+
+
---
+++ src/main/java/com/yearup/dealership/Program.java
@@ -0,0 +1,9 @@
+package com.yearup.dealership;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+
+    }
+}
