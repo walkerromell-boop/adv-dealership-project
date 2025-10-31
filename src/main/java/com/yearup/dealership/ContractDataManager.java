12d3fcf1db36c4c8e6da3698e16b29eeda0b8536
Romell Walker <walkerromell@gmail.com>
2025-10-31 16:21:26 -0400

    add contract data class

---
+++ src/main/java/com/yearup/dealership/ContractDataManager.java
@@ -0,0 +1,76 @@
+package com.yearup.dealership;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ContractDataManager {
+
+    public void saveContract(Contract contract) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("contracts.csv", true))) {
+            Vehicle vehicle = contract.getVehicle();
+
+            String contractType;
+            if (contract instanceof SalesContract) {
+                contractType = "SALE";
+            } else {
+                contractType = "LEASE";
+            }
+
+            String baseInfo = String.format("%s|%s|%s|%s|%d|%s|%s|%s|%s|%s|%d|%.2f",
+                    contractType,
+                    contract.getContractDate(),  // no DateTimeFormatter
+                    contract.getCustomerName(),
+                    contract.getCustomerEmail(),
+                    vehicle.getVin(),
+                    vehicle.getYear(),
+                    vehicle.getMake(),
+                    vehicle.getModel(),
+                    vehicle.getVehicleType(),
+                    vehicle.getColor(),
+                    vehicle.getOdometer(),
+                    vehicle.getPrice()
+
+            );
+            if (contract instanceof SalesContract) {
+                SalesContract sale = (SalesContract) contract;
+
+                String financedText;
+                if (sale.getMonthlyPayment() > 0) {
+                    financedText = "YES";
+                } else {
+                    financedText = "NO";
+                }
+
+                String line = String.format(
+                        "%s|%.2f|100.00|%.2f|%.2f|%s|%.2f",
+                        baseInfo,
+                        vehicle.getPrice() * 0.05, // sales tax
+                        sale.getProcessingFee(),
+                        sale.getTotalPrice(),
+                        financedText,
+                        sale.getMonthlyPayment()
+                );
+
+                writer.write(line + "\n");
+
+            } else if (contract instanceof LeaseContract) {
+                LeaseContract lease = (LeaseContract) contract;
+
+                String line = String.format(
+                        "%s|%.2f|%.2f|%.2f|%.2f",
+                        baseInfo,
+                        lease.getVehicle().getPrice() * 0.5,  // expected ending value
+                        lease.getVehicle().getPrice() * 0.07, // lease fee
+                        lease.getTotalPrice(),
+                        lease.getMonthlyPayment()
+                );
+
+                writer.write(line + "\n");
+            }
+            writer.flush();
+            System.out.println("Contract saved successfully");
+        }
+    }
+}
--- src/main/java/com/yearup/dealership/LeaseContract.java
+++ src/main/java/com/yearup/dealership/LeaseContract.java
@@ -3,8 +3,8 @@ package com.yearup.dealership;
 import java.time.LocalDate;
 
 public class LeaseContract extends Contract {
-    private double expectedEndingVaule;
-    private double leasingFee;
+    private double expectedEndingValue;
+    private double leasingFee = 0.07;
 
     public LeaseContract(LocalDate contractDate, String customerName, String customerEmail, Vehicle vehicles) {
         super(contractDate, customerName, customerEmail, vehicles);
@@ -12,26 +12,31 @@ public class LeaseContract extends Contract {
 
     public LeaseContract(LocalDate contractDate, String customerName, String customerEmail, Vehicle vehicles, double expectedEndingVaule, double leasingFee) {
         super(contractDate, customerName, customerEmail, vehicles);
-        this.expectedEndingVaule = expectedEndingVaule;
+        this.expectedEndingValue = expectedEndingVaule;
         this.leasingFee = leasingFee;
     }
 
     @Override
     public double getTotalPrice() {
-        return 0;
+
+        double leasingFee = getVehicle().getPrice() * leasingFee;
+        return expectedEndingValue + leasingFee;
     }
 
     @Override
     public double getMonthlyPayment() {
-        return 0;
+        double total = getTotalPrice();
+        double rate = 0.04 / 12;
+        int months = 36;
+        return (total * rate) / (1 - Math.pow(1 + rate, -months));
     }
 
-    public double getExpectedEndingVaule() {
-        return expectedEndingVaule;
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
     }
 
-    public void setExpectedEndingVaule(double expectedEndingVaule) {
-        this.expectedEndingVaule = expectedEndingVaule;
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
     }
 
     public double getLeasingFee() {
@@ -40,10 +45,6 @@ public class LeaseContract extends Contract {
 
     public void setLeasingFee(double leasingFee) {
         this.leasingFee = leasingFee;
-    }
-
-    public double getTotalPrice() {
-        return getTotalPrice();
     }
 
 
